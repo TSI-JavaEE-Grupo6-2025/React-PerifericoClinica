7f1ea0c53e68aed36060def79a6c5f5a263b55c0
MasliahDev99 <felipe_dev99@outlook.es>
2025-10-27 03:46:07 -0300

    fix: Se modifico el request para authenticación para manejar multi roles

--- periferico-clinica/src/types/Auth.ts
+++ periferico-clinica/src/types/Auth.ts
@@ -13,6 +13,7 @@ export interface UserHcen { // usado en UserCredentials Value Object
     email: string;
     password: string;
     tenantId: string; // cambio domain por tenantId fecha 20/10/2025 01:05 am
+    role?: UserRole;
 }
 
 
