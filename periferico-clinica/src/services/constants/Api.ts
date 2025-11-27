641786dd9100049db513357cca2567e4414eac22
MasliahDev99 <felipe_dev99@outlook.es>
2025-11-26 21:11:48 -0300

    Se modifico el import.meta.env.MODE por import.meta.env.DEV
    -> Con esta mejora, vite detecta que esta en produccion y utiliza la url de heroku.

--- periferico-clinica/src/services/constants/Api.ts
+++ periferico-clinica/src/services/constants/Api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-
-const apiBaseUrl = import.meta.env.MODE === 'production' 
+// Si esta en modo desarrollo, se utilizara localhost
+const apiBaseUrl = import.meta.env.DEV
   ? 'http://localhost:8081'
   : (import.meta.env.VITE_API_BASE_URL || 'https://periferico-clinica-g6-2132940f4c98.herokuapp.com');
 
